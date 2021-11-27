9b7f7562d13974269b033322f396fb863fb9257e
Renato Assis <renato.assis@seidor.com.br>
2021-11-27 20:09:51 -0300

    test: tests for scan and get

--- test/aws/dynamodb.test.ts
+++ test/aws/dynamodb.test.ts
@@ -51,3 +51,55 @@ test.serial('DynamoDB put CASE 2', async t => {
         message: 'payload required',
     });
 });
+
+test.serial('DynamoDB scan CASE 1', async t => {
+    const scanSpy = sinon
+        .stub(AWS.DynamoDB.DocumentClient.prototype, 'scan')
+        .returns(<any>{
+            promise: () => {
+                return Promise.resolve({
+                    Count: 1,
+                    Items: [],
+                });
+            },
+        });
+
+    await dynamodb.getAll();
+
+    const expectedParams = {
+        TableName: 'user',
+    };
+
+    t.true(scanSpy.calledWith(expectedParams));
+});
+
+test.serial('DynamoDB get CASE 1', async t => {
+    const getSpy = sinon.stub(AWS.DynamoDB.DocumentClient.prototype, 'get').returns(<
+        any
+    >{
+        promise: () => {
+            return Promise.resolve({});
+        },
+    });
+
+    const event = {
+        pathParameters: {
+            pk: 'pk',
+        },
+    };
+
+    await dynamodb.getItemByPk(<any>event);
+
+    const expectedParams = {
+        TableName: 'user',
+        Key: { pk: 'pk' },
+    };
+
+    t.true(getSpy.calledWith(expectedParams));
+});
+
+test.serial('DynamoDB get CASE 2', async t => {
+    await t.throwsAsync(() => dynamodb.getItemByPk(<any>{ pathParameters: {} }), {
+        message: 'pk is required',
+    });
+});
