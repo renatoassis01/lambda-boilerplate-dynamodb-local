9a96472c7400c3892811fc4672dee3217a429218
Renato Assis <renato.assis@seidor.com.br>
2021-11-27 12:28:37 -0300

    test: test buildresponse

---
+++ test/utils/builder.test.ts
@@ -0,0 +1,17 @@
+import test from 'ava';
+import { builderResponse } from '../../src/utils/builder';
+
+test('test for builderResponse', t => {
+    const expectResult = {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+        },
+        body: '{"message":"Hello World"}',
+    };
+
+    const result = builderResponse(200, { message: 'Hello World' });
+
+    t.deepEqual(result, expectResult);
+});
